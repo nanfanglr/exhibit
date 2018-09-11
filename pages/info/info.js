5e05e5aae0360755e23e9011db1497d46b9475ff
liurui <1513093228@qq.com>
2018-09-11 09:13:17 +0800

    添加了静态样式到陈列知识页面

--- pages/info/info.js
+++ pages/info/info.js
@@ -7,7 +7,121 @@ Page({
   data: {
     currentTab: 0,
     isShowKN: true,
-    knowledges: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+    knowledges: [{
+        url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      }, {
+        url: "https://ws1.sinaimg.cn/large/0065oQSqly1fuh5fsvlqcj30sg10onjk.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://ws1.sinaimg.cn/large/0065oQSqly1fuo54a6p0uj30sg0zdqnf.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://ww1.sinaimg.cn/large/0065oQSqly1fszxi9lmmzj30f00jdadv.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://cdn.it120.cc/apifactory/2017/09/18/bce3fa5808e4ba3586ca04e03d4b5ed4.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+      {
+        url: "https://cdn.it120.cc/apifactory/2017/09/18/3e1d9b4200ba4ea4fe0c607d5b67b2aa.jpg",
+        title: "玩转8个销售连带提升,达成指标更靠谱",
+        read: 3,
+        praise: 6,
+        msg: 99,
+      },
+    ],
+
+    infos: [{
+        title: "品牌速递",
+        data: [{
+            url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg",
+            title: "玩转8个销售连带提升,达成指标更靠谱",
+            read: 3,
+            praise: 6,
+            msg: 99,
+          },
+          {
+            url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg",
+            title: "玩转8个销售连带提升,达成指标更靠谱",
+            read: 3,
+            praise: 6,
+            msg: 99,
+          },
+          {
+            url: "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg",
+            title: "玩转8个销售连带提升,达成指标更靠谱",
+            read: 3,
+            praise: 6,
+            msg: 99,
+          }
+        ]
+      },
+      {
+        title: "终端故事",
+        data: [{
+          url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftf1snjrjuj30se10r1kx.jpg",
+          title: "玩转8个销售连带提升,达成指标更靠谱",
+          read: 3,
+          praise: 6,
+          msg: 99,
+        },
+        {
+          url: "https://ww1.sinaimg.cn/large/0065oQSqly1ftzsj15hgvj30sg15hkbw.jpg",
+          title: "玩转8个销售连带提升,达成指标更靠谱",
+          read: 3,
+          praise: 6,
+          msg: 99,
+        },
+        {
+          url: "https://ws1.sinaimg.cn/large/0065oQSqly1fubd0blrbuj30ia0qp0yi.jpg",
+          title: "玩转8个销售连带提升,达成指标更靠谱",
+          read: 3,
+          praise: 6,
+          msg: 99,
+        }
+        ]
+      },
+    ],
   },
 
   /**
@@ -79,7 +193,7 @@ Page({
     }
   },
   //点击切换商品列表
-  clickTabPro: function (e) {
+  clickTabPro: function(e) {
     var that = this;
     if (this.data.currentTab === e.target.dataset.current) {
       return false;
